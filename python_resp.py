8c69dd85806ba391f6c56f4a4f152de0193a3a2e
zhuzeyang <zeyangzhu@163.com>
2018-06-19 22:16:16 +0800

    Initial commit

---
+++ python_resp.py
@@ -0,0 +1,47 @@
+#！/usr/bin/env.python
+# _*_ coding:utf-8 _*_
+
+
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+
+# 执行API调用并存储响应
+url = 'http://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print('status code:', r.status_code)
+
+# 将API存储在一个变量中
+response_dict = r.json()
+print('total repositories:', response_dict['total_count'])
+
+# 搜索有关仓库的信息
+repo_dicts = response_dict['items']
+
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+
+    plot_dict = {'value': repo_dict['stargazers_count'],
+                 'label': repo_dict['description'], }
+    plot_dicts.append(plot_dict)
+
+# 可视化
+my_style = LS('#333366', base_style=LCS)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 10
+my_config.truncate_label = 15
+my_config.show_y_guides = True
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-starred Python Projects on Github'
+chart.x_labels = names
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
